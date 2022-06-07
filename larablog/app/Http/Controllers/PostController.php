fec6664f49a0390f3cb48a2286e6838241af6579
Gavin Harrold <gsh3009@rit.edu>
2022-06-07 16:05:23 -0400

    eager loading to reduce queries

--- larablog/app/Http/Controllers/PostController.php
+++ larablog/app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index() {
 
-        $posts = Post::paginate(20); //all posts
+        $posts = Post::with(['user', 'likes'])->paginate(20); //all posts
         return view('posts.index', [
             'posts' => $posts
         ]);
