dc2fe135fa9b68cc2dbe68f22ca1aea606510876
Gavin Harrold <gsh3009@rit.edu>
2022-06-09 10:11:16 -0400

    fixed pagination css and implemented db seeding

--- larablog/app/Http/Controllers/PostController.php
+++ larablog/app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
 
     public function index() {
 
-        $posts = Post::latest()->with(['user', 'likes'])->paginate(20); //all posts
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(3); //all posts
         return view('posts.index', [
             'posts' => $posts
         ]);
--- larablog/database/seeders/DatabaseSeeder.php
+++ larablog/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use Database\Factories\PostFactory;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
+        User::factory(10)
+            ->has(Post::factory()->count(3))
+            ->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
